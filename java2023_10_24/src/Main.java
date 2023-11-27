16c71015aaf5922892a3a5c5295d9dfc07f5278c
Han <kismetfor33455@163.com>
2023-11-27 17:22:43 +0800

    first commit

---
+++ java2023_10_24/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("A");
+        System.out.println(100);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+}
