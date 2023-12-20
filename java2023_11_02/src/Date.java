8ba2fd41b478014d57bec341ab0e7b5774f0dfa8
Han <kismetfor33455@163.com>
2023-12-20 20:16:00 +0800

    类和对象

--- java2023_11_02/src/Date.java
+++ java2023_11_02/src/Date.java
@@ -2,6 +2,20 @@ public class Date {
     public int year;
     public int month;
     public int date;
+    public Date() {
+        this(2002,12,12);
+    }
+    public Date(int year, int month, int date){
+        this.year = year;
+        this.month = month;
+        this.date = date;
+    }
+
+    public Date(int year, int date) {
+        this.year = year;
+        this.date = date;
+    }
+
     public void setDate(int year, int month, int date){
         this.year = year;
         this.month = month;
@@ -13,9 +27,7 @@ public class Date {
 
     public static void main(String[] args) {
         Date date1 = new Date();
-        date1.setDate(2021, 12,21);
         date1.printDate();
-        Date date2 = new Date();
-        date2.setDate(1234,1,23);
+
     }
 }
