e7e34f5fe99af4d4d2cb0daf26c11b7d72075b13
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-02-14 22:17:29 +0800

    明天加油干

--- 数据结构/2023-12-02链表完+栈队列/project/src/StackDemo/MyStack.java
+++ 数据结构/2023-12-02链表完+栈队列/project/src/StackDemo/MyStack.java
@@ -1,5 +1,26 @@
 package StackDemo;
 
 public class MyStack {
+    public int[] elem;
+    public int usedSize;
+    public static final int DEFAULT_CAPACITY = 10;
+    public MyStack() {
+        this.elem = new int[DEFAULT_CAPACITY];
+    }
+    public void push(int val) {
+        if (isFull()) {
+        }
+    }
+
+    public boolean isFull() {
+        return usedSize== elem.length;
+    }
+
+    public int pop() {
+
+    }
+    public int peek() {
+
+    }
 
 }
