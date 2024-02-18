4110c393e11125018de7a0053db1c18f405c2d0a
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-02-18 21:28:26 +0800

    2023-12-08二叉树第二节课

--- 数据结构/2023-12-08二叉树/project/src/demo/BinaryTree.java
+++ 数据结构/2023-12-08二叉树/project/src/demo/BinaryTree.java
@@ -57,4 +57,113 @@ public class BinaryTree {
         postOrder(node.right);
         System.out.print(node.val+" ");
     }
+    public static int nodeSize;
+
+    /**
+     * 获取树中节点的个数：遍历思路
+     */
+    void size(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        nodeSize++;
+        size(root.left);
+        size(root.right);
+    }
+
+    /**
+     * 获取节点的个数：子问题的思路
+     *
+     * @param root
+     * @return
+     */
+    int size2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return size2(root.right)+size2(root.left)+1;
+    }
+
+
+    /*
+     获取叶子节点的个数：遍历思路
+     */
+    public static int leafSize = 0;
+
+    void getLeafNodeCount1(TreeNode root) {
+        if (root==null) {
+            return;
+        }
+        getLeafNodeCount1(root.left);
+        getLeafNodeCount1(root.right);
+        if (root.left==null&& root.right==null) {
+            leafSize++;
+        }
+    }
+
+    /*
+     获取叶子节点的个数：子问题
+     */
+    int getLeafNodeCount2(TreeNode root) {
+        if (root==null) {
+            return 0;
+        }
+        if (root.left==null&& root.right==null) {
+            return 1;
+        }
+        return getLeafNodeCount2(root.left)+getLeafNodeCount2(root.right);
+    }
+
+    /*
+    获取第K层节点的个数
+     */
+    int getKLevelNodeCount(TreeNode root, int k) {
+        if (root==null) {
+            return 0;
+        }
+        if (k==1) {
+            return 1;
+        }
+        return getKLevelNodeCount(root.left, k-1)+
+                getKLevelNodeCount(root.right, k-1);
+    }
+
+    /*
+     获取二叉树的高度
+     时间复杂度：O(N)
+     */
+    int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left),
+                getHeight(root.right))+1;
+    }
+
+
+    // 检测值为value的元素是否存在
+    TreeNode find(TreeNode root, char val) {
+        if (root==null) {
+            return null;
+        }
+        if (root.val==val) {
+            return root;
+        }
+        TreeNode findLeft = find(root.left, val);
+        if (findLeft!=null) {
+            return findLeft;
+        }
+        return find(root.right, val);
+    }
+
+    //层序遍历
+    void levelOrder(TreeNode root) {
+
+    }
+
+
+    // 判断一棵树是不是完全二叉树
+    boolean isCompleteTree(TreeNode root) {
+        return true;
+    }
 }
--- 数据结构/2023-12-08二叉树/project/src/demo/Test.java
+++ 数据结构/2023-12-08二叉树/project/src/demo/Test.java
@@ -4,13 +4,38 @@ public class Test {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         BinaryTree.TreeNode root = binaryTree.creatTree();
-        System.out.println(root.val);
+        /*System.out.println(root.val);
         binaryTree.preOrder(root);
         System.out.println();
 
         binaryTree.inOrder(root);
         System.out.println();
 
-        binaryTree.postOrder(root);
+        binaryTree.postOrder(root);*/
+
+        /*binaryTree.size(root);
+        System.out.println(BinaryTree.nodeSize);*/
+
+        System.out.print("节点个数: ");
+        System.out.println(binaryTree.size2(root));
+
+        System.out.print("叶子节点个数: ");
+        /*binaryTree.getLeafNodeCount1(root);
+        System.out.println(BinaryTree.leafSize);*/
+        System.out.println(binaryTree.getLeafNodeCount2(root));
+
+        System.out.print("第3层节点数: ");
+        System.out.println(binaryTree.getKLevelNodeCount(root, 3));
+
+        System.out.print("获取二叉树的高度: ");
+        System.out.println(binaryTree.getHeight(root));
+
+        System.out.print("检测值为value的元素是否存在: ");
+        BinaryTree.TreeNode node = binaryTree.find(root, 'K');
+        if (node==null) {
+            System.out.println("null");
+        } else {
+            System.out.println(node.val);
+        }
     }
 }
