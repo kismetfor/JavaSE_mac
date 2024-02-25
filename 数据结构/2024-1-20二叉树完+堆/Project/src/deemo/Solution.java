3e52f06fb4694207dbbe710f8e7e15b1002c23c5
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-02-25 22:56:17 +0800

    2024-1-20二叉树完+堆

---
+++ 数据结构/2024-1-20二叉树完+堆/Project/src/deemo/Solution.java
@@ -0,0 +1,55 @@
+package deemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root==null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur!=null) {
+            stack.push(cur);
+            list.add(cur.val);
+            cur = cur.left;
+        }
+    }
+
+    public String tree2str(TreeNode root) {
+        StringBuilder sbm = new StringBuilder();
+        tree2strChild(root, sbm);
+        return sbm.toString();
+    }
+    public void tree2strChild(TreeNode root, StringBuilder sbm) {
+        if (root==null) {
+            return;
+        }
+        sbm.append(root.val);
+        if (root.left!=null) {
+            sbm.append('(');
+            tree2strChild(root.left, sbm);
+            sbm.append(')');
+        } else {
+            if (root.right!=null) {
+                //左空右不空
+                sbm.append("()");
+                sbm.append('(');
+                tree2strChild(root.right, sbm);
+                sbm.append(')');
+            } else {
+                //左右都为空
+                return;
+            }
+        }
+        if (root.right!=null && root.left!=null) {
+            //左右都不为空
+            sbm.append('(');
+            tree2strChild(root.right, sbm);
+            sbm.append(')');
+        }
+    }
+}
---
+++ 数据结构/2024-1-20二叉树完+堆/Project/src/deemo/Test.java
@@ -0,0 +1,7 @@
+package deemo;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
