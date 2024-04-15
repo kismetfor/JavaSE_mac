81c75fa96ef1d3d90bc3c4adb99a7b3eb3f18510
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-04-15 16:18:01 +0800

    第一节课: cd

---
+++ 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo1.java
@@ -0,0 +1,6 @@
+public class ThreadDemo1 {
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+        object.wait();
+    }
+}
---
+++ 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo4.java
@@ -0,0 +1,2 @@
+public class ThreadDemo4 {
+}
