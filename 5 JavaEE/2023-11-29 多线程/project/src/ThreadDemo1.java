7d56d8d1e5b0054a4f42cfeb8a39abf48f52a95c
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-04-16 22:49:32 +0800

    11-30多线程单例模式懒汉式

--- 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo1.java
+++ 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo1.java
@@ -1,6 +1,32 @@
 public class ThreadDemo1 {
     public static void main(String[] args) throws InterruptedException {
         Object object = new Object();
-        object.wait();
+        Thread t1 = new Thread(()->{
+            synchronized (object) {
+                System.out.println("t1 wait begin");
+                try {
+                    //需要确保先执行wait 再执行notify
+                    Thread.sleep(500000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("t1 wait end");
+            }
+        });
+        Thread t2 = new Thread(()->{
+            synchronized (object) {
+                System.out.println("t2 notify begin");
+                object.notify();
+                System.out.println("t2 notify end");
+
+            }
+        });
+        t1.start();
+        t2.start();
     }
 }
--- 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo2.java
+++ 5 JavaEE/2023-11-29 多线程/project/src/ThreadDemo2.java
@@ -1,2 +1,19 @@
+class Singleton {
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+    private Singleton() {
+
+    }
+}
 public class ThreadDemo2 {
+
+    public static void main(String[] args) throws InterruptedException {
+//        Singleton s = new Singleton();
+        Singleton s = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s==s2);
+    }
 }
