d723aab58689eb198c93528c270eb5c4a81bc333
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-04-21 22:41:38 +0800

    快结束了

--- 5 JavaEE/2024-01-15多线程/project/src/Main6.java
+++ 5 JavaEE/2024-01-15多线程/project/src/Main6.java
@@ -1,2 +1,16 @@
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main6 {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        list.add(1, "D");
+
+        for(String s : list) {
+            System.out.print(s+" ");
+        }
+    }
 }
