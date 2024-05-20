27a71d4ae23bac316c1c04b945654690604751e0
kismetfor <kismetfor33455@163.com>
2024-05-20 10:52:23 +0800

    MVC02完: 初识session和Cookie

---
+++ 6.JavaEE进阶/24-03-12springmvc/springmvc/src/main/java/com/bite/springmvc/demos/SessionController.java
@@ -0,0 +1,46 @@
+package com.bite.springmvc.demos;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+@RequestMapping("/session")
+@RestController
+public class SessionController {
+    @RequestMapping("/getC")
+    public String getCookie(HttpServletRequest request, HttpServletResponse response) {
+        //获取参数
+//        String name = request.getParameter("name");
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null) {
+            Arrays.stream(cookies).forEach(ck -> System.out.println(ck.getName()+": "+ck.getValue()));
+        }
+        return "获取Cookie";
+    }
+
+    @RequestMapping("/getC2")
+    public String getCookie2(@CookieValue("bite") String bite) {
+        return "获取Cookie2 bite: "+bite;
+    }
+
+    @RequestMapping("/setSess")
+    public String setSess(HttpServletRequest request) {
+        //从Cookie中获取到了sessionID, 根据session获取session对象, 如果没有获取到 创建一个session对象
+        HttpSession session = request.getSession();
+        session.setAttribute("name", "zhangsan");
+        return "设置session成功";
+    }
+
+    @RequestMapping("/getSess")
+    public String getSess(HttpServletRequest request) {
+        //从Cookie中获取到了sessionID, 根据session获取session对象
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        return "获取Session中 name: "+name;
+    }
+}
