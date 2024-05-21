e9cf58d85319f2881d02794401f2ec29734b7c34
kismetfor <kismetfor33455@163.com>
2024-05-21 15:42:35 +0800

    留言墙 感觉都不是很懂 需要听第二遍应该

---
+++ 6.JavaEE进阶/24-03-12springmvc/springmvc/src/main/java/com/bite/springmvc/demos/MessageController.java
@@ -0,0 +1,41 @@
+package com.bite.springmvc.demos;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//http://127.0.0.1:8080/message/publish
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    private List<MessageInfo> messageInfos = new ArrayList<>();
+
+    /**
+     * 获取留言信息
+     * @return
+     */
+    @RequestMapping("/getList")
+    public List<MessageInfo> getList() {
+        return messageInfos;
+    }
+
+    /**
+     * 发布留言信息
+     * @param messageInfo
+     * @return
+     */
+    @RequestMapping("/publish")
+    public Boolean publish(MessageInfo messageInfo) {
+        System.out.println("接收参数, messageInfo: " + messageInfo);
+        if (!StringUtils.hasLength(messageInfo.getFrom()) ||
+                !StringUtils.hasLength(messageInfo.getTo()) ||
+                !StringUtils.hasLength(messageInfo.getSay())) {
+            return false;
+        }
+        messageInfos.add(messageInfo);
+        return true;
+    }
+}
---
+++ 6.JavaEE进阶/24-03-12springmvc/springmvc/src/main/java/com/bite/springmvc/demos/MessageInfo.java
@@ -0,0 +1,11 @@
+package com.bite.springmvc.demos;
+
+import lombok.Data;
+
+@Data
+public class MessageInfo {
+    private String from;
+    private String to;
+    private String say;
+
+}
