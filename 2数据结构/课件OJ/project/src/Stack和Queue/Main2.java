c5d647534a28c05657049e32429dee1fb20b1b10
kismetfor <kismetfor33455@163.com>
2024-05-28 23:07:50 +0800

    写不下去

--- 2数据结构/课件OJ/project/src/Stack和Queue/Main2.java
+++ 2数据结构/课件OJ/project/src/Stack和Queue/Main2.java
@@ -1,4 +1,38 @@
 package Stack和Queue;
 
+import java.util.Stack;
+
+/**
+ * 逆波兰表达式求值
+ */
 public class Main2 {
+    public int evalRPN(String[] tokens) {
+        int ret = 0;
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (stack.size() < 2) {
+                stack.push(tokens[i]);
+            } else {
+                if (tokens[i].equals("+")) {
+                    int a = Integer.parseInt(stack.pop());
+                    int b = Integer.parseInt(stack.pop());
+
+                } else if (tokens[i].equals("-")) {
+
+                } else if (tokens[i].equals("*")) {
+
+                } else if (tokens[i].equals("/")) {
+
+                } else {
+                    stack.push(tokens[i]);
+                }
+            }
+        }
+        //先入栈
+
+        //遇到运算符出栈
+
+        //再入栈
+
+    }
 }
