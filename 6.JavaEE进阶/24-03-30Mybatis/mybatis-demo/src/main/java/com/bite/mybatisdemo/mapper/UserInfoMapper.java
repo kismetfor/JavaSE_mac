b801790a5df0399160aad55e31f65c809aca89e4
kismetfor <kismetfor33455@163.com>
2024-05-29 22:59:38 +0800

    Mybatis参数传递

--- 6.JavaEE进阶/24-03-30Mybatis/mybatis-demo/src/main/java/com/bite/mybatisdemo/mapper/UserInfoMapper.java
+++ 6.JavaEE进阶/24-03-30Mybatis/mybatis-demo/src/main/java/com/bite/mybatisdemo/mapper/UserInfoMapper.java
@@ -2,6 +2,7 @@ package com.bite.mybatisdemo.mapper;
 
 import com.bite.mybatisdemo.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,6 +11,11 @@ public interface UserInfoMapper {
     @Select("select * from userinfo")
     List<UserInfo> getUserInfoAll();
 
+    @Select("select * from userinfo where delete_flag = #{deleteFlag}")
+    List<UserInfo> getUserInfo1(Integer flag);
+
+    @Select("select * from userinfo where delete_flag = #{deleteFlag} and gender = #{gender}")
+    List<UserInfo> getUserInfo2(@Param("deleteFlag") Integer flag, @Param("gender") Integer gender);
 
 
 }
--- 6.JavaEE进阶/24-03-30Mybatis/mybatis-demo/src/test/java/com/bite/mybatisdemo/mapper/UserInfoMapperTest.java
+++ 6.JavaEE进阶/24-03-30Mybatis/mybatis-demo/src/test/java/com/bite/mybatisdemo/mapper/UserInfoMapperTest.java
@@ -1,13 +1,38 @@
 package com.bite.mybatisdemo.mapper;
 
+import com.bite.mybatisdemo.model.UserInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class UserInfoMapperTest {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
 
     @Test
     void getUserInfoAll() {
+    }
+
+    @Test
+    void getUserInfo1() {
+    }
+
+    @Test
+    void testGetUserInfo1() {
+//        List<UserInfo> userInfos = userInfoMapper.getUserInfo1(0);
+//        System.out.println(userInfos);
+        System.out.println(userInfoMapper.getUserInfo1(0));
+    }
+
+
+    @Test
+    void testGetUserInfo2() {
+        System.out.println(userInfoMapper.getUserInfo2(0, 1));
+
     }
 }
