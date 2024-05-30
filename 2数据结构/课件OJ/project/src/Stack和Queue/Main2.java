377c9dbef8fc191978f10de69d9e4fbe2ddd93db
kismetfor <kismetfor33455@163.com>
2024-05-30 18:00:18 +0800

    逆波兰表达式求值 今天第一题

--- 2数据结构/课件OJ/project/src/Stack和Queue/Main2.java
+++ 2数据结构/课件OJ/project/src/Stack和Queue/Main2.java
@@ -6,33 +6,44 @@ import java.util.Stack;
  * 逆波兰表达式求值
  */
 public class Main2 {
-    public int evalRPN(String[] tokens) {
+    public static void main(String[] args) {
+        String[] tokens = {"2","1","+","3","*"};
+//        String[] tokens ={"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        System.out.println(evalRPN(tokens));
+    }
+    public static int evalRPN(String[] tokens) {
         int ret = 0;
         Stack<String> stack = new Stack<>();
         for (int i = 0; i < tokens.length; i++) {
             if (stack.size() < 2) {
                 stack.push(tokens[i]);
             } else {
-                if (tokens[i].equals("+")) {
+                //容量足以进行运算的时候
+                //分两种情况
+                //1. 运算符
+                //2. 数字
+                if (tokens[i].equals("+") ||
+                        tokens[i].equals("-") ||
+                        tokens[i].equals("*") ||
+                        tokens[i].equals("/")) {
                     int a = Integer.parseInt(stack.pop());
                     int b = Integer.parseInt(stack.pop());
-
-                } else if (tokens[i].equals("-")) {
-
-                } else if (tokens[i].equals("*")) {
-
-                } else if (tokens[i].equals("/")) {
-
+                    int c = 0;
+                    switch (tokens[i]) {
+                        case "+": c = a + b; break;
+                        case "-": c = b - a; break;
+                        case "*": c = a * b; break;
+                        case "/": c = b / a; break;
+                        default:
+                            System.out.println("错误");
+                    }
+                    stack.push(Integer.toString(c));
                 } else {
                     stack.push(tokens[i]);
                 }
             }
         }
-        //先入栈
-
-        //遇到运算符出栈
-
-        //再入栈
-
+        ret = Integer.parseInt(stack.pop());
+        return ret;
     }
 }
