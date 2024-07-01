1efaa7177fafbb324d4f5f0717ff3cbdadfea2af
Han <kismetfor33455@163.com>
2024-07-01 22:21:15 +0800

    对称二叉树

--- 2数据结构/课件OJ/project/src/二叉树/Main6.java
+++ 2数据结构/课件OJ/project/src/二叉树/Main6.java
@@ -2,16 +2,18 @@ package 二叉树;
 
 public class Main6 {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-        if (root.left != null && root.right != null) {
-            if (root.left.val == root.right.val) {
-                return isSymmetric(root.left) && isSymmetric(root.right);
-            }
-            return false;
-        }
-        if (root.left == null && root.right == null) {
-            return true;
+        return dfs(root.left, root.right);
+    }
+
+    public boolean dfs(TreeNode leftTree, TreeNode rightTree) {
+        if (leftTree==null && rightTree == null) return true;
+        if (leftTree==null || rightTree == null) return false;
+        if (leftTree.val == rightTree.val) {
+            return dfs(leftTree.left, rightTree.right) && dfs(leftTree.right, rightTree.left);
         }
         return false;
+    }
+    public void init() {
+
     }
 }
