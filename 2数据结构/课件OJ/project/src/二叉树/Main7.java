d92b3d7aa5b26acea60840301fdc28c9a3d0a26d
Han <kismetfor33455@163.com>
2024-07-03 23:52:34 +0800

    二叉树前序遍历创建二叉树

--- 2数据结构/课件OJ/project/src/二叉树/Main7.java
+++ 2数据结构/课件OJ/project/src/二叉树/Main7.java
@@ -1,7 +1,25 @@
 package 二叉树;
 
+import com.sun.source.tree.Tree;
+
 public class Main7 {
     public static void main(String[] args) {
 
+    }
+    public static Tree chuangjian(String string) {
+        TreeNode root = null;
+        char[] chars = string.toCharArray();
+        for (char ch:
+             chars) {
+            if (ch != '#') {
+                if (root == null) {
+                    root.val = ch;
+                } else {
+
+                }
+            } else {
+
+            }
+        }
     }
 }
