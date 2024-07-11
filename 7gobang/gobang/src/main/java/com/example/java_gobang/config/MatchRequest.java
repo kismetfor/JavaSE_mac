406d7c3bd56e2bc53a8875e47b4ab3bd051bf239
Han <kismetfor33455@163.com>
2024-07-11 23:24:06 +0800

    匹配

---
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/config/MatchRequest.java
@@ -0,0 +1,4 @@
+package com.example.java_gobang.config;
+
+public class MatchRequest {
+}
---
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/config/MatchResponse.java
@@ -0,0 +1,5 @@
+package com.example.java_gobang.config;
+//这是表示websocket的
+public class MatchResponse {
+
+}
