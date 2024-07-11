c2690cfb041e414cdb56cc2d3864e7524afacc13
Han <kismetfor33455@163.com>
2024-07-11 10:50:30 +0800

    创建MatchAPI 引入WebSocket

---
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/api/MatchAPI.java
@@ -0,0 +1,32 @@
+package com.example.java_gobang.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+@Component
+public class MatchAPI extends TextWebSocketHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
--- 7gobang/gobang/src/main/java/com/example/java_gobang/config/WebSocketConfig.java
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/config/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.example.java_gobang.config;
 
+import com.example.java_gobang.api.MatchAPI;
 import com.example.java_gobang.api.TestAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -13,8 +15,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Autowired
     private TestAPI testAPI;
+    @Autowired
+    private MatchAPI matchAPI;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(testAPI, "/test");
+        registry.addHandler(matchAPI, "/findMatch").
+                addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 }
