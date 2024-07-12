571b9b23a3b2b32ad682cfd03f16d74d498904c9
Han <kismetfor33455@163.com>
2024-07-12 22:52:41 +0800

    用户登录Post改Get

--- 7gobang/gobang/src/main/java/com/example/java_gobang/api/UserAPI.java
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/api/UserAPI.java
@@ -3,6 +3,7 @@ package com.example.java_gobang.api;
 import com.example.java_gobang.model.User;
 import com.example.java_gobang.model.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class UserAPI {
         // 如果能找到匹配的用户并且密码一致, 就确认为登陆成功
         User user = userMapper.selectByName(username);
         if (user == null || !user.getPassword().equals(password)) {
+            System.out.println("登录失败");
             return new User(); //这是空对象 说明登录失败
         }
         //这里的true表示如果会话存在, 那么返回session, 不存在创建一个新的会话返回
@@ -48,12 +50,21 @@ public class UserAPI {
         }
 
     }
-    @PostMapping("/userInfo")
+
+    /**
+     * 发生的错误[WARN ] - Resolved [org.springframework.web.HttpRequestMethodNotSupportedException: Request method 'GET' not supported]  登录后跳转到http://127.0.0.1:8080/game_hall.html
+     * 获取用户信息失败!
+     * 这里是因为之前写的是PostMapping 改为GetMapping
+     * @param request
+     * @return
+     */
+    @GetMapping("/userInfo")
     @ResponseBody
     public Object getUserInfo(HttpServletRequest request) {
         try {
             HttpSession httpSession = request.getSession(false);
             User user = (User) httpSession.getAttribute("user");
+            User newUser = userMapper.selectByName(user.getUsername());
             return user;
         } catch (NullPointerException e) {
             return new User();
--- 7gobang/gobang/src/main/java/com/example/java_gobang/game/OnlineUserManager.java
+++ 7gobang/gobang/src/main/java/com/example/java_gobang/game/OnlineUserManager.java
@@ -4,21 +4,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class OnlineUserManager {
     //键值对存储游戏用户是否在线大厅状态
-    private HashMap<Integer, WebSocketSession> gameHall = new HashMap<>();
+    private ConcurrentHashMap<Integer, WebSocketSession> gameHall = new ConcurrentHashMap<>();
 
     public void enterGameHall(int userId, WebSocketSession webSocketSession) {
         gameHall.put(userId, webSocketSession);
     }
 
-    public void exitGameHall(int userId, WebSocketSession webSocketSession) {
-        gameHall.remove(userId, webSocketSession);
+    public void exitGameHall(int userId) {
+        gameHall.remove(userId);
     }
 
 
-    public WebSocketSession getFromGameHall(int userId, WebSocketSession webSocketSession) {
+    public WebSocketSession getFromGameHall(int userId) {
         return gameHall.get(userId);
     }
 }
